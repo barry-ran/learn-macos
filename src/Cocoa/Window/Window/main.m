f673c6322ebaff23ea99cb13064f995d39927b69
rankun <rankun@bytedance.com>
2019-11-05 13:17:32 +0800

    feat: add Window

---
+++ src/Cocoa/Window/Window/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Window
+//
+//  Created by barry on 2019/11/5.
+//  Copyright © 2019 barry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
