0faed2cbf9462245401a66b7060fbe79f9d16657
rankun <rankun@bytedance.com>
2019-11-05 13:17:32 +0800

    feat: add Window

---
+++ src/Cocoa/Window/Window/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Window
+//
+//  Created by barry on 2019/11/5.
+//  Copyright © 2019 barry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
