61c70bfaf64d0eb6f2a853db3702985700a422e0
rankun <rankun@bytedance.com>
2020-03-02 19:58:40 +0800

    feat: add EventPostToPSN

---
+++ src/Cocoa/EventPostToPSN/EventPostToPSN/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EventPostToPSN
+//
+//  Created by barry on 2020/3/2.
+//  Copyright © 2020 barry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ src/Cocoa/EventPostToPSN/EventPostToPSN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EventPostToPSN
+//
+//  Created by barry on 2020/3/2.
+//  Copyright © 2020 barry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
