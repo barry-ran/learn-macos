e1066c569dd73fa859c2f7dad9a7571a603deb13
rankun <rankun@bytedance.com>
2021-04-25 18:21:01 +0800

    feat: add window visible check

---
+++ src/Cocoa/CheckWindowOcclusion/CheckWindowOcclusion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CheckWindowOcclusion
+//
+//  Created by barry on 2021/4/25.
+//  Copyright © 2021 barry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
