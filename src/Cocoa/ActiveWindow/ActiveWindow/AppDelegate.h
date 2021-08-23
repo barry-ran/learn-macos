2f006b96474bc7d42035aede136f88116a88c9da
rankun <rankun@bytedance.com>
2021-08-23 20:20:22 +0800

    feat: add active window

---
+++ src/Cocoa/ActiveWindow/ActiveWindow/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ActiveWindow
+//
+//  Created by barry on 2021/8/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ src/Cocoa/ActiveWindow/ActiveWindow/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  ActiveWindow
+//
+//  Created by barry on 2021/8/23.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
