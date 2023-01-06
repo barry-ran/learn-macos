8bc022f7b5bae1f1f5aa95111d58eed751be3a12
rankun <kun.ran@mihoyo.com>
2023-01-07 00:08:53 +0800

    feat: add custom titlebar

---
+++ src/Cocoa/CustomTitlebar/CustomTitlebar/MainWindowController.h
@@ -0,0 +1,16 @@
+//
+//  MainWindowController.h
+//  CustomTitlebar
+//
+//  Created by 冉坤(Barry.R) on 2023/1/6.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/Cocoa/CustomTitlebar/CustomTitlebar/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  CustomTitlebar
+//
+//  Created by 冉坤(Barry.R) on 2023/1/6.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
