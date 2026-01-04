6bac9ef88d08cb7e97310d34b77802b536f91cd3
kun.ran <kun.ran@mihoyo.com>
2026-01-04 20:56:21 +0800

    feat: 补充LSUIElement和setActivationPolicy和NSWindowStyleMaskNonactivatingPanel对显示在全屏窗口上面的影响

--- src/Cocoa/ShowAboveFullScreenWindow/ShowAboveFullScreenWindow/AppDelegate.m
+++ src/Cocoa/ShowAboveFullScreenWindow/ShowAboveFullScreenWindow/AppDelegate.m
@@ -14,34 +14,73 @@
 
 @implementation AppDelegate
 NSPanel* panel;
-// 开了LSUIElement以后就不需要NSWindowStyleMaskNonactivatingPanel了
+// 最终总结：
+// 辅助进程（开了LSUIElement或者setActivationPolicy设置了NSApplicationActivationPolicyAccessory）不需要设置NSWindowStyleMaskNonactivatingPanel也可以让NSPanel和NSWindow显示在其他全屏窗口上面
+// 普通进程只能让有NSWindowStyleMaskNonactivatingPanel的NSPanel显示在其他全屏窗口上面，NSWindow不支持设置NSWindowStyleMaskNonactivatingPanel
+// LSUIElement在info.plist中静态设置辅助进程和普通进程
+// setActivationPolicy可以运行后动态设置辅助进程和普通进程
+// 只要NSPanel/NSWindow（不设置NSWindowStyleMaskNonactivatingPanel）创建时是辅助进程，即使之后改为普通进程，也还是可以显示在其他全屏窗口上面
+// 为什么这么不喜欢NSWindowStyleMaskNonactivatingPanel？因为有很多轻击触摸板没有焦点的bug
+
 // 在info.plist里面开启LSUIElement yes以后这里也同步放开测试效果
-#define LSUIElement
+// #define LSUIElement
+#ifdef LSUIElement
+// 设置LSUIElement以后，会导致进程没有dock图标&窗口没有菜单栏&全屏红绿灯
+// https://developer.apple.com/documentation/appkit/nsapplication/activationpolicy-swift.enum?language=objc
+// 可以在进程启动时通过设置setActivationPolicy
+// NSApplicationActivationPolicyRegular动态把进程改为普通进程，这样dock图标这些就回来了
+// 但是普通进程创建非NSWindowStyleMaskNonactivatingPanel的NSPanel无法显示在其他全屏窗口上面了
+// 但是只要在创建NSPanel之前临时把进程设置为NSApplicationActivationPolicyAccessory，创建完NSPanel再设置回NSApplicationActivationPolicyRegular
+// 就可以保证依然是普通进程，但是NSPanel不需要NSWindowStyleMaskNonactivatingPanel也可以悬浮在其他全屏窗口上面
+// 动态设置进程属性的缺点是dock图标会消失再出现，次数多了还会导致永远消失或者变得很小，只能执行killallDock命令重启dock
+// #define LSUIElement_dock
+#endif
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        panel = [[NSPanel alloc] initWithContentRect:NSMakeRect(300, 300, 500, 500)
-                                           styleMask:NSWindowStyleMaskTitled|NSWindowStyleMaskClosable|NSWindowStyleMaskMiniaturizable|NSWindowStyleMaskResizable
-                                                       backing:NSBackingStoreBuffered
-                                                         defer:YES];
-        // 重要！！！：如果在设置NSWindowStyleMaskNonactivatingPanel style之前设置下面的behavior，会无效
-        // 并且导致即使设置NSWindowStyleMaskNonactivatingPanel style之后再次设置behavior，依然会无效
-        // 这也是为什么在qt中即使设置了NSWindowCollectionBehaviorFullScreenAuxiliary始终不能显示在全屏窗口上面的原因
-        // [panel setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces|NSWindowCollectionBehaviorFullScreenAuxiliary];
-        // 参考自telegram https://github.com/Etersoft/telegram-desktop/blob/3e4b3801e4d0c65bf06b73b48982d82fd6be318f/tdesktop/Telegram/Patches/qtbase_5_6_2.diff#L860
+#ifdef LSUIElement_dock
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+#endif
+
+  dispatch_after(
+      dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)),
+      dispatch_get_main_queue(), ^{
+#ifdef LSUIElement_dock
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
+#endif
+        panel = [[NSPanel alloc]
+            initWithContentRect:NSMakeRect(300, 300, 500, 500)
+                      styleMask:NSWindowStyleMaskTitled |
+                                NSWindowStyleMaskClosable |
+                                NSWindowStyleMaskMiniaturizable |
+                                NSWindowStyleMaskResizable
+                        backing:NSBackingStoreBuffered
+                          defer:YES];
+    // macos 13以下的额外要求：
+    // 重要！！！：在macos 13以下，如果在设置NSWindowStyleMaskNonactivatingPanel style之前设置下面的behavior，会无效
+    // 并且导致即使设置NSWindowStyleMaskNonactivatingPanel style之后再次设置behavior，依然会无效
+    // 这也是为什么macos 13以下在qt中即使按下面条件设置了style和behavior也始终不能显示在全屏窗口上面的原因
+    // 因为qt内部会早早设置behavior，导致后面我们应用层设置NSWindowStyleMaskNonactivatingPanel没效果，只能通过修改qt源码解决
+    // [panel setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces|NSWindowCollectionBehaviorFullScreenAuxiliary];
+    // 参考自telegram
+    // https://github.com/Etersoft/telegram-desktop/blob/3e4b3801e4d0c65bf06b73b48982d82fd6be318f/tdesktop/Telegram/Patches/qtbase_5_6_2.diff#L860
+    // 解决qt bug还有一个方法：在macos13以下，创建窗口之前动态设置进程为辅助进程，创建完窗口再设置回普通进程
     
-        // 显示在其他app的全屏窗口上面的必要条件：
+    // macos 13及以上没有这个问题，只要满足下面6个条件
+    // 显示在其他app的全屏窗口上面的必要条件：
 #ifndef LSUIElement
         // 1. 必须NSPanel
         // 2. NSWindowStyleMaskNonactivatingPanel 窗口不激活app
-        [panel setStyleMask:panel.styleMask|NSWindowStyleMaskNonactivatingPanel];
+        [panel
+            setStyleMask:panel.styleMask | NSWindowStyleMaskNonactivatingPanel];
 #endif
         // 3. NSWindowCollectionBehaviorCanJoinAllSpaces 可以显示在其他屏幕空间
         // 4. NSWindowCollectionBehaviorFullScreenAuxiliary 可以显示在全屏窗口屏幕空间
-        [panel setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces|NSWindowCollectionBehaviorFullScreenAuxiliary];
-    
-        // z序高一点
+        [panel setCollectionBehavior:
+                   NSWindowCollectionBehaviorCanJoinAllSpaces |
+                   NSWindowCollectionBehaviorFullScreenAuxiliary];
+
+        // 5. z序高一点
         [panel setLevel:kCGStatusWindowLevel];
-        // 失焦不关闭
+        // 6. 失焦不关闭
         [panel setReleasedWhenClosed:YES];
         [panel setHidesOnDeactivate:NO];
         [panel center];
@@ -61,19 +100,25 @@ NSPanel* panel;
         [panel.contentView addSubview:panelLabel];
 
         // 创建文本输入框
-        NSTextField* textField = [[NSTextField alloc] initWithFrame:NSMakeRect(20, 80, 260, 24)];
+        NSTextField *textField =
+            [[NSTextField alloc] initWithFrame:NSMakeRect(20, 80, 260, 24)];
         [textField setPlaceholderString:@"输入些内容"];
         [panel.contentView addSubview:textField];
-            
-            // 创建关闭按钮
-        NSButton* closeButton = [[NSButton alloc] initWithFrame:NSMakeRect(100, 30, 100, 30)];
+
+        // 创建关闭按钮
+        NSButton *closeButton =
+            [[NSButton alloc] initWithFrame:NSMakeRect(100, 30, 100, 30)];
         [closeButton setTitle:@"关闭"];
         [closeButton setTarget:self];
         [closeButton setAction:@selector(closePanel:)];
         [panel.contentView addSubview:closeButton];
-    });
-    
-    // 测试NSWindow (NSWindow不支持NSWindowStyleMaskNonactivatingPanel，所以不开LSUIElement的场景下NSWindow不能显示在其他app的全屏窗口上)
+
+#ifdef LSUIElement_dock
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+#endif
+      });
+
+  // 测试NSWindow(NSWindow不支持NSWindowStyleMaskNonactivatingPanel，所以不开LSUIElement的场景下NSWindow不能显示在其他app的全屏窗口上)
 #ifndef LSUIElement
     [_window setStyleMask:panel.styleMask|NSWindowStyleMaskNonactivatingPanel];
 #endif
