c6afa673755364fde8099d88c9a6bf56f701bb2a
kun.ran <kun.ran@mihoyo.com>
2026-01-04 18:12:48 +0800

    feat: 给NSWindow和NSPanel窗口增加明显标识

--- src/Cocoa/ShowAboveFullScreenWindow/ShowAboveFullScreenWindow/AppDelegate.m
+++ src/Cocoa/ShowAboveFullScreenWindow/ShowAboveFullScreenWindow/AppDelegate.m
@@ -16,7 +16,7 @@
 NSPanel* panel;
 // 开了LSUIElement以后就不需要NSWindowStyleMaskNonactivatingPanel了
 // 在info.plist里面开启LSUIElement yes以后这里也同步放开测试效果
-//#define LSUIElement
+#define LSUIElement
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         panel = [[NSPanel alloc] initWithContentRect:NSMakeRect(300, 300, 500, 500)
@@ -46,7 +46,20 @@ NSPanel* panel;
         [panel setHidesOnDeactivate:NO];
         [panel center];
         [panel orderFront:nil];
-        
+
+        // 在panel中间显示文字NSPanel
+        NSTextField *panelLabel =
+            [[NSTextField alloc] initWithFrame:NSMakeRect(150, 250, 200, 40)];
+        [panelLabel setStringValue:@"NSPanel"];
+        [panelLabel setBezeled:NO];
+        [panelLabel setDrawsBackground:NO];
+        [panelLabel setEditable:NO];
+        [panelLabel setSelectable:NO];
+        [panelLabel setAlignment:NSTextAlignmentCenter];
+        [panelLabel setFont:[NSFont systemFontOfSize:24
+                                              weight:NSFontWeightBold]];
+        [panel.contentView addSubview:panelLabel];
+
         // 创建文本输入框
         NSTextField* textField = [[NSTextField alloc] initWithFrame:NSMakeRect(20, 80, 260, 24)];
         [textField setPlaceholderString:@"输入些内容"];
@@ -60,12 +73,32 @@ NSPanel* panel;
         [panel.contentView addSubview:closeButton];
     });
     
-    // 测试NSWindow (NSWindow不支持NSWindowStyleMaskNonactivatingPanel，所以NSWindow不能显示在其他app的全屏窗口上)
+    // 测试NSWindow (NSWindow不支持NSWindowStyleMaskNonactivatingPanel，所以不开LSUIElement的场景下NSWindow不能显示在其他app的全屏窗口上)
 #ifndef LSUIElement
     [_window setStyleMask:panel.styleMask|NSWindowStyleMaskNonactivatingPanel];
 #endif
     [_window setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces|NSWindowCollectionBehaviorFullScreenAuxiliary];
     [_window setLevel:kCGStatusWindowLevel];
+
+    // 在_window中间显示文字NSWindow
+    NSTextField *windowLabel =
+        [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 200, 40)];
+    [windowLabel setStringValue:@"NSWindow"];
+    [windowLabel setBezeled:NO];
+    [windowLabel setDrawsBackground:NO];
+    [windowLabel setEditable:NO];
+    [windowLabel setSelectable:NO];
+    [windowLabel setAlignment:NSTextAlignmentCenter];
+    [windowLabel setFont:[NSFont systemFontOfSize:24 weight:NSFontWeightBold]];
+    // 设置自动布局约束，让标签居中
+    [windowLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_window.contentView addSubview:windowLabel];
+    [NSLayoutConstraint activateConstraints:@[
+      [windowLabel.centerXAnchor
+          constraintEqualToAnchor:_window.contentView.centerXAnchor],
+      [windowLabel.centerYAnchor
+          constraintEqualToAnchor:_window.contentView.centerYAnchor]
+    ]];
 }
 
 - (void)closePanel:(id)sender {
